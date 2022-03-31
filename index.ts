cc907e1c472e2fa3a59ff91aeafc4b777ed62159
KnowsCount <knowscount@gmail.com>
2022-03-31 11:33:12 +0800

    feat: hashtag before hex values

--- index.ts
+++ index.ts
@@ -5,21 +5,25 @@ const hexBlend = (
 	blend: number
 ) => {
 	const wrongChar: RegExp = /[^0-9a-f]/i
-
+	const hex: RegExp = /^#?([0-9a-f]{3}|[0-9a-f]{6})$/i
+	if (hex.test(colour1) && hex.test(colour2)) {
+		colour1 = colour1.replace('#', '')
+		colour2 = colour2.replace('#', '')
+	}
 	if (
 		colour1.length !== 6 ||
 		colour2.length !== 6 ||
 		wrongChar.test(colour1) ||
 		wrongChar.test(colour2)
 	)
-		return 'input format is wrong. please use 6 hexadecimal characters without `#` in front.'
+		return 'input format is wrong.'
 
 	const getcolourFromRange = (range: number[], res: number, arg: number) =>
 		((range[1] - range[0]) / res) * arg + range[0]
 
 	const [RGB1, RGB2]: any = [colour1.match(/.{2}/g), colour2.match(/.{2}/g)]
 
-	return [
+	return `#${[
 		~~getcolourFromRange(
 			[parseInt(RGB1[0], 16), parseInt(RGB2[0], 16)],
 			res,
@@ -37,7 +41,7 @@ const hexBlend = (
 		),
 	]
 		.map((el) => el.toString(16).padStart(2, '0'))
-		.join('')
+		.join('')}`
 }
 
 export default hexBlend
